db7b3713a77b8b8903ee9092bd31147867bf2ead
Anurag Makineni <anurag.makineni@gmail.com>
2016-03-30 01:29:41 -0400

    moved files to make raphael happy

---
+++ controller/src/motors.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include <controller/Motors.h>
+#include <geometry_msgs/Twist.h>
+#include <Eigen/Geometry>
+
+void motors_cb(const controller::Motors::ConstPtr& msg)
+{
+    ROS_INFO("Got motors message!");
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "motors");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("/motors", 1, motors_cb);
+
+  ros::spin();
+
+  return 0;
+}
